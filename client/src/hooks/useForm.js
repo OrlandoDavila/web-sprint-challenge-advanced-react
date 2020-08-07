fd6df45b9f055836f579bdef5b0be71690827c4f
Orlando Davila <odavila0308@gmail.com>
2020-08-07 12:42:18 -0400

    useForm.js complete

--- client/src/hooks/useForm.js
+++ client/src/hooks/useForm.js
@@ -1,1 +1,19 @@
+import CheckoutForm from "../components/CheckoutForm"
 // write your custom hook here to control your checkout form
+
+export const useForm = (intValue) => {
+    const [values, setValues] = CheckoutForm(intValue);
+
+    const handleChanges = e => {
+        setValues({
+            ...values, [e.target.name]: e.target.value
+        })
+    }
+
+    const clearForm = e => {
+        e.preventDefault();
+        setValues(intValue);
+    }
+
+    retuen [values, handleChanges, clearForm]
+}
